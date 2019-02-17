392f2ccf89ca7ac2a63a97c674bf78e8bc8dc6e7
tayna <taynajesus@gmail.com>
2019-02-17 17:19:53 -0400

    teste imagem pmz

---
+++ src/app/app.component.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+
+  photos = [
+    { 
+      url: 'http://www.grupopmz.com/wp-content/uploads/2016/09/Centro-de-Distribui%C3%A7%C3%A3o-2.jpg', 
+      description: 'PMZ'
+    },
+    { 
+      url: 'http://www.grupopmz.com/wp-content/uploads/2016/09/Centro-de-Distribui%C3%A7%C3%A3o-2.jpg', 
+      description: 'PMZ'
+    },
+    { 
+      url: 'http://www.grupopmz.com/wp-content/uploads/2016/09/Centro-de-Distribui%C3%A7%C3%A3o-2.jpg', 
+      description: 'PMZ'
+    }         
+  ];  
+
+}
